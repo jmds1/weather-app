2d08869b745b0df8c51025e31a59814efcc71485
Nelly Lopez <nlopez53@ucmerced.edu>
2025-01-24 11:37:54 -0800

    improved git layout;searchBar

--- weather-main/src/app/page.tsx
+++ weather-main/src/app/page.tsx
@@ -5,37 +5,32 @@ export default function Home() {
   return (
     <>
  
-      <h1>Welcome to My Next.js App</h1>
-      <p>This is the homepage of your application built with the App Router.</p>
    
-   <div className="container  m-auto grid grid-cols-3 gap-4">
-        <div className="tile bg-teal-500">
-          <h1 className="tile-marker">ONE</h1>
+   
+   <div className="container m-auto grid sm:grid-cols-1 md:grid-cols-2 md:grid-row-3  gap-4 ">
+        <div className="tile bg-teal-500 row-start-1 rounded-md p-6">
+          <h1 className="tile-marker font-bold ">City Name</h1>
+          <p>Chance of Rain: %</p>
+          <p>Degrees in Farenheit</p>
+          <p className="">Image Goes here! </p>
         </div>
-        <div className="tile bg-amber-500">
-          <h1 className="tile-marker">TWO</h1>
+        <div className="tile bg-amber-500 row-start-2 rounded-md p-6">
+          <h1 className="tile-marker">Today's Forecast will go Here!</h1>
+          <ul>
+            <li>6:00am </li>
+            <li>9:00am</li>
+            <li>12:00pm </li>
+            <li>6:00pm</li>
+            <li>9:00pm</li>
+          </ul>
         </div>
-        <div className="tile bg-yellow-500">
-          <h1 className="tile-marker">THREE</h1>
+        <div className="tile bg-yellow-500 row-start-3 rounded-md p-6">
+          <h1 className="tile-marker">Air Conditions will go here!</h1>
         </div>
-        <div className="tile bg-lime-600">
-          <h1 className="tile-marker">FOUR</h1>
+        <div className="tile bg-lime-600 col-start-2 row-span-3 rounded-md p-6">
+          <h1 className="tile-marker">7-Day Forecast goes here!</h1>
         </div>
-        <div className="tile bg-green-600">
-          <h1 className="tile-marker">FIVE</h1>
-        </div>
-        <div className="tile bg-emerald-500">
-          <h1 className="tile-marker">SIX</h1>
-        </div>
-        <div className="tile bg-teal-500">
-          <h1 className="tile-marker">SEVEN</h1>
-        </div>
-        <div className="tile bg-purple-500">
-          <h1 className="tile-marker">EIGHT</h1>
-        </div>
-        <div className="tile bg-pink-500">
-          <h1 className="tile-marker">NINE</h1>
-        </div>
+      
       </div>
    </>
   );
