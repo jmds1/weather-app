e48ebdebb0f9380e58b42ce4561134c36cb0a80c
Nelly Lopez <nlopez53@ucmerced.edu>
2025-01-28 15:59:39 -0800

    remove link

--- weather-main/src/app/layout.tsx
+++ weather-main/src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import SearchBar from "../../components/SearchBar";
 import NavBar from "../../components/NavBar";
-import Link from "next/link";
+
 
 import "./globals.css";
 
