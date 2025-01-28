77685e94c443cf981f900c25f8ef343e204cd74d
Nelly Lopez <nlopez53@ucmerced.edu>
2025-01-28 15:57:29 -0800

    made nav and search into components

--- weather-main/tailwind.config.ts
+++ weather-main/tailwind.config.ts
@@ -5,6 +5,7 @@ export default {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}"
   ],
