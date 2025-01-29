8a47e9f617efe922ae84eb9ed631f2555fb42ef7
Nelly Lopez <nlopez53@ucmerced.edu>
2025-01-29 13:57:48 -0800

    grids are responsive

--- weather-main/src/app/page.tsx
+++ weather-main/src/app/page.tsx
@@ -7,14 +7,16 @@ export default function Home() {
  
    
    
-   <div className="container m-auto grid sm:grid-cols-1 md:grid-cols-2 md:grid-row-3  gap-4 ">
-        <div className="tile bg-teal-500 row-start-1 rounded-md p-6">
+   <div className="container m-auto grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 gap-4 ">
+    <div className="grid gap-4">
+        <div className="tile bg-teal-500  rounded-md p-6">
+          
           <h1 id="city-name" className="tile-marker font-bold ">City Name</h1>
           <p id="chance-rain">Chance of Rain: %</p>
           <p id="degrees">Degrees in Farenheit</p>
           <p className="">Image Goes here! </p>
         </div>
-        <div className="tile bg-amber-500 row-start-2 rounded-md p-6">
+        <div className="tile bg-amber-500 md:col-span-1 rounded-md p-6">
           {/* eslint-disable-next-line react/no-unescaped-entities */}
           <h1 className="tile-marker">Today&#39;s Forecast will go Here!</h1>
           <ul>
@@ -25,10 +27,11 @@ export default function Home() {
             <li>9:00pm</li>
           </ul>
         </div>
-        <div className="tile bg-yellow-500 row-start-3 rounded-md p-6">
+        <div className="tile bg-yellow-500 rounded-md p-6">
           <h1 className="tile-marker">Air Conditions will go here!</h1>
         </div>
-        <div className="tile bg-lime-600 col-start-2 row-span-3 rounded-md p-6">
+        </div>
+        <div className="tile bg-lime-600 sm:col-span-1 md:col-span-1 rounded-md p-6">
           <h1 className="tile-marker">7-Day Forecast goes here!</h1>
         </div>
       
