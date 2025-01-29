cb135beab0f6f7c8b87d3c3d6655fbdc47f9cb2e
Nelly Lopez <nlopez53@ucmerced.edu>
2025-01-29 11:42:45 -0800

    search/nav responsive

--- weather-main/tailwind.config.ts
+++ weather-main/tailwind.config.ts
@@ -16,13 +16,7 @@ export default {
         foreground: "var(--foreground)",
       },
     },
-    screens: {
-      sm: '640px',
-      md: '768px',
-      lg: '1024px',
-      xl: '1280px',
-      '2xl': '1536px',
-    },
+    
   },
   darkMode: "class",
   plugins: [heroui()]
