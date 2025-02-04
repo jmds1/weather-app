62696e0a5a48d3e5348bc0b28f9de4a3e105a9ca
Nelly Lopez <nlopez53@ucmerced.edu>
2025-02-03 16:03:40 -0800

    modified eslint

--- weather-main/src/app/layout.tsx
+++ weather-main/src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import SearchBar from "../../components/SearchBar";
+
 import NavBar from "../../components/NavBar";
 
 
