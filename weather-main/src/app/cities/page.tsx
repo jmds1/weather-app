c41e804519e3d650a862dcb9652bec6361b2ad9c
Nelly Lopez <nlopez53@ucmerced.edu>
2025-02-05 11:29:39 -0800

    added pages and links

---
+++ weather-main/src/app/cities/page.tsx
@@ -0,0 +1,3 @@
+export default function Page(){
+    return <h1>UNDER CONSTRUCTION....CITY WILL GO HERE</h1>
+}
---
+++ weather-main/src/app/map/page.tsx
@@ -0,0 +1,3 @@
+export default function Page(){
+    return <h1>UNDER CONSTRUCTION....MAP WILL GO HERE</h1>
+}
