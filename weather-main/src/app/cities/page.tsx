5a826fcb42b08e21bc77dd43a31119c9440d2495
Nelly Lopez <nlopez53@ucmerced.edu>
2025-03-12 16:32:48 -0700

    workign on cities page

--- weather-main/src/app/cities/page.tsx
+++ weather-main/src/app/cities/page.tsx
@@ -1,3 +1,18 @@
 export default function Page(){
-    return <h1>UNDER CONSTRUCTION....CITY WILL GO HERE</h1>
+    return (
+<div className="flex flex-row m-auto w-full  items-center">
+{/* Need to write an if-statment here: if not loaded then only show LA */}
+   
+   {/* LA will go here as default */}
+    <div className="flex flex-row m-4 w-2/3 border border-blue-400 rounded-sm ">First City of LA
+    
+    
+    </div>
+
+    <div className="flex flex-row m-4 w-1/3 rounded-sm ">Sirst City of LA</div>
+
+</div>
+
+    )
+
 }
